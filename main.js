b23ef181952baffdfcc251331e459d3e3e5f6be6
acl13 <128934431+acl13@users.noreply.github.com>
2024-06-25 11:00:37 -0400

    validate US postal codes; display error message if invalid input

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 const form = document.getElementById('form');
 const email = document.getElementById("email");
 const emailError = document.querySelector("#email + span.error");
+const country = document.getElementById('country');
+const zip = document.getElementById('zip');
+const zipError = document.querySelector("#zip + span.error");
 const submit = document.getElementById('submit');
 
 email.addEventListener("input", () => {
@@ -10,6 +13,17 @@ email.addEventListener("input", () => {
       showError();
     }
 });
+
+zip.addEventListener("input", () => {
+    if ((country.value === 'US') && 
+    (/\d{5}([ \-]\d{4})?/.test(zip.value) === false)) {
+        zipError.textContent = "Please enter a valid US ZIP code.";
+    } else if (zip.validity.valid) {
+      zipError.textContent = ""; 
+    } else {
+      showError();
+    }
+  });
 
 submit.addEventListener('click', (event) => {
     event.preventDefault();
@@ -23,5 +37,7 @@ function showError() {
       emailError.textContent = "Please enter an email address.";
     } else if (email.validity.typeMismatch) {
       emailError.textContent = "Entered value needs to be an email address.";
+    } else if (zip.validity.valueMissing) {
+        zipError.textContent = "Please enter your postal code.";
     } 
 }
